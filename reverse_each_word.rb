3d9bcbfb8d513dd34300712bf21a7a386b4265fa
nathalia <nathaliatroina@gmail.com>
2022-02-21 12:56:54 -0500

    commit tree 9e2e50ccc21a0dffe9c6ac2c6567146530d5cc0c

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,11 +2,27 @@ require 'pry'
 # Write your code here
 
 def reverse_each_word(sentence)
+   words = sentence.split
+   reversed_words = []
+
+   words.each do |word|
+    reversed_words << word.reverse
+   end
+   reversed_words.join(" ")
+end
+
+
+puts reverse_each_word("Hello there, and how are you?")
+
+
+
+
+
+
+
+def reverse_each_word(sentence)
   sentence_arr = sentence.split
   reversedArr = sentence_arr.map(&:reverse)
   reversedArr.join(" ")
   # sentence.split.map(&:reverse).join(" ")
 end
-
-binding.pry
-0
